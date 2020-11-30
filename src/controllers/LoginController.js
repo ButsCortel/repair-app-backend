fb7af7b8b095fc93eb2298a5a1b031f98b41a65d
unknown <cortel.butch.kleina@gmail.com>
2020-12-01 01:08:59 +0800

    Updated jwt.sign

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -3,45 +3,49 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 module.exports = {
-    async login(req, res) {
-        try {
-            const {
-                email,
-                password
-            } = req.body;
-            if (!email || !password) {
-                return res.status(400).json({
-                    message: "Required field/s missing!"
-                })
-            }
-            const user = await User.findOne({
-                email
-            }).select("+password");
-            if (!user) {
-                return res.status(401).json({
-                    message: "Email or Password does not match!"
-                })
-            } else if (user && (await bcrypt.compare(password, user.password))) {
-                const userResponse = {
-                    _id: user._id,
-                    email: user.email,
-                    firstName: user.firstName,
-                    lastName: user.lastName
-                };
-                const token = jwt.sign(userResponse, process.env.SECRET);
-                return res.header("auth-token", token).sendStatus(200);
-            } else {
-                return res.status(401).json({
-                    message: "Email or Password does not match!"
-                })
-            }
-
-        } catch (error) {
-            console.log(error)
-            return res.status(500).json({
-                message: "Error while trying to log in! " + error
-            })
-        }
-
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      if (!email || !password) {
+        return res.status(400).json({
+          message: "Required field/s missing!",
+        });
+      }
+      const user = await User.findOne({
+        email,
+      }).select("+password");
+      if (!user) {
+        return res.status(401).json({
+          message: "Email or Password does not match!",
+        });
+      } else if (user && (await bcrypt.compare(password, user.password))) {
+        const userResponse = {
+          _id: user._id,
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+        };
+        return jwt.sign(
+          { user: userResponse },
+          process.env.SECRET,
+          (err, token) => {
+            if (err)
+              return res.status(401).json({
+                message: "Email or Password does not match!",
+              });
+            res.json({ token: token, user: userResponse });
+          }
+        );
+      } else {
+        return res.status(401).json({
+          message: "Email or Password does not match!",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "Error while trying to log in! " + error,
+      });
     }
-}
+  },
+};
