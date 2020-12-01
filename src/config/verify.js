60ed00e5bc71b0b5904ceea7d34b05b37a2a41ac
unknown <cortel.butch.kleina@gmail.com>
2020-12-02 01:18:11 +0800

    updated verify

--- src/config/verify.js
+++ src/config/verify.js
@@ -1,23 +1,25 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/Users")
+const User = require("../models/Users");
 
 const verify = async (req, res, next) => {
-    const token = req.header("auth-token");
-    if (!token) return res.status(401).json({
-        message: "Unauthorized!"
+  const token = req.header("auth-token");
+  if (!token)
+    return res.status(401).json({
+      message: "Unauthorized!",
     });
-    try {
-        const verified = jwt.verify(token, process.env.SECRET);
-        const user = await User.findById(verified._id);
-        if (!user) return res.status(401).json({
-            message: "Unauthorized!"
-        })
-        req.user = verified;
-        next();
-    } catch (error) {
-        return res.status(401).json({
-            message: "Unauthorized!"
-        })
-    }
-}
+  try {
+    const verified = jwt.verify(token, process.env.SECRET);
+    const user = await User.findById(verified.user._id);
+    if (!user)
+      return res.status(401).json({
+        message: "Unauthorized!",
+      });
+    req.user = verified;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorized!",
+    });
+  }
+};
 module.exports = verify;
